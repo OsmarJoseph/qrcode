e96c25d422e0b1d04f22f1795711ce63020f8c36
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-08-16 05:11:19 -0300

    add function loadUpdates

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from './store/Provider';
 import Routes from "./routes"
+import * as Updates from 'expo-updates';
+
 
 export default function App () {
+  useEffect(() => {
+    async function loadUpdates () {
+      try {
+        const update = await Updates.checkForUpdateAsync();
+        if (update.isAvailable) {
+          await Updates.fetchUpdateAsync();
+          // ... notify user of update ...
+          await Updates.reloadAsync();
+        }
+      } catch (e) {
+        // handle or log error
+      }
+    }
+    loadUpdates()
+  }, [])
   return (
     <Provider>
       <Routes />
