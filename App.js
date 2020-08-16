1fec34ad5815cfce1ca4ec0f00ddd37186e3d994
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-08-16 04:33:31 -0300

    testing CI

--- App.js
+++ App.js
@@ -1,52 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import {
-  createStackNavigator,
-  CardStyleInterpolators,
-} from '@react-navigation/stack';
-import Scanner from './src/components/Scanner';
-import ScannedTextView from './src/components/ScannedTextView';
 import { Provider } from './store/Provider';
-import { backgroundColor, textColor } from './src/utils/colors';
-const Stack = createStackNavigator();
+import Routes from "./routes"
 
-export default function App() {
+export default function App () {
   return (
     <Provider>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-          }}
-        >
-          <Stack.Screen
-            name="Scanner"
-            component={Scanner}
-            options={{
-              headerStyle: {
-                backgroundColor,
-              },
-              headerTitleStyle: {
-                color: textColor,
-              },
-            }}
-          />
-          <Stack.Screen
-            name="ScannedTextView"
-            component={ScannedTextView}
-            options={{
-              title: 'Código analisado',
-              headerStyle: {
-                backgroundColor,
-              },
-              headerTitleStyle: {
-                color: textColor,
-              },
-              headerTintColor: '#9F81AA',
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Routes />
     </Provider>
   );
 }
---
+++ routes.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
+import Scanner from './src/components/Scanner';
+import ScannedTextView from './src/components/ScannedTextView';
+import { backgroundColor, textColor } from './src/utils/colors';
+
+export default function Routes () {
+  const Stack = createStackNavigator();
+  return (
+
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      >
+        <Stack.Screen
+          name="Scanner"
+          component={Scanner}
+          options={{
+            headerStyle: {
+              backgroundColor,
+            },
+            headerTitleStyle: {
+              color: textColor,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="ScannedTextView"
+          component={ScannedTextView}
+          options={{
+            title: 'Código analisado',
+            headerStyle: {
+              backgroundColor,
+            },
+            headerTitleStyle: {
+              color: textColor,
+            },
+            headerTintColor: '#9F81AA',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+
+}
