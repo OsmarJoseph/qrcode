010b8126f280c0695f766f3964faa6bc1c012101
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-08-19 00:45:06 -0300

    testing CD

--- routes.js
+++ routes.js
@@ -19,7 +19,7 @@ export default function Routes () {
         }}
       >
         <Stack.Screen
-          name="Scanner Testing"
+          name="Scanner"
           component={Scanner}
           options={{
             headerStyle: {
